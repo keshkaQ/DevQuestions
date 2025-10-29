f047233d654ba2a89725fc4eab96c2f095b4a766
keshkaQ <kirill.banar44@gmail.com>
2025-10-29 10:21:08 +0300

    done with domain models

---
+++ DevQuestions/src/DevQuestions.Domain/Reports/Status.cs
@@ -0,0 +1,6 @@
+namespace DevQuestions.Domain.Report;
+
+public enum Status
+{
+    Open, InProgress, Resolved, Dismissed
+}
---
+++ DevQuestions/src/DevQuestions.Domain/Tags/Tag.cs
@@ -0,0 +1,7 @@
+namespace DevQuestions.Domain.Tags;
+
+public class Tag
+{
+    public Guid Id { get; set; }
+    public required string Name { get; set; }
+}
